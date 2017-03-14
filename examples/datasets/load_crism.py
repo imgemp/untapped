Now load data from umass website. Fixed DEMO_mnist bug. Removed irrelevant CRISM KL stats.

examples/datasets/load_crism.py
@@ -10,28 +10,16 @@ from os.path import join
 
 from sklearn.decomposition import PCA
 
-
-def get_endmembers(path='examples/datasets/crism/endmembers.txt'):
-    return np.loadtxt(path)
+from untapped.utilities import load_url
 
 
-def get_data(path='examples/datasets/crism/multiAngle_measurements'):
-    names = [['ternaryMix1_position1.mat', 'mix1Samp_pos1'],
-             ['ternaryMix1_position2.mat', 'mix1Samp_pos2'],
-             ['ternaryMix1_samplesAngled.mat', 'mix1Samp_angle']]
-    suffix = '_Abun'
-    x_arr = []
-    y_arr = []
-    for group in names:
-        data = loadmat(join(path, group[0]))
-        x_arr.append(data[group[1]])
-        y_arr.append(data[group[1] + suffix])
+def get_endmembers(dataset='examples/datasets/crism/crism.pkl.gz'):
+    x, y, endmembers = load_url('https://people.cs.umass.edu/~imgemp/datasets/crism.pkl.gz',dataset)
+    return endmembers
 
-    x = x_arr[0]
-    y = y_arr[0]
-    for i in range(1, len(x_arr)):
-        x = np.append(x, x_arr[i], axis=0)
-        y = np.append(y, y_arr[i], axis=0)
+
+def get_data(dataset='examples/datasets/crism/crism.pkl.gz'):
+    x, y, _ = load_url('https://people.cs.umass.edu/~imgemp/datasets/crism.pkl.gz',dataset)
 
     names = ['olivine','diopside','bytownite']
     colors = ['r','g','b']

examples/datasets/load_libs.py
@@ -6,6 +6,8 @@ from sklearn.model_selection import StratifiedKFold
 
 from matplotlib.pyplot import cm
 
+from untapped.utilities import load_url
+
 # Mask for LIBS channels
 ALAMOS_MASK = np.zeros(6144, dtype=bool)
 ALAMOS_MASK[110:1994] = True
@@ -13,13 +15,18 @@ ALAMOS_MASK[2169:4096] = True
 ALAMOS_MASK[4182:5856] = True
 
 
-def load_process_data(big=True,trial=0,hardCV=True,cv_param=3,remove_mean=True,log_x=False,eps=1e-7):
+def load_process_data(dataset='examples/datasets/libs/libs.pkl.gz',big=True,trial=0,hardCV=True,cv_param=3,remove_mean=True,log_x=False,eps=0.):
+
+    small_means, big_means, cal_data = load_url('https://people.cs.umass.edu/~imgemp/datasets/libs.pkl.gz',dataset)
 
     # unlabeled dataset
-    unsup_x = load_mars_means(big=big).clip(eps,np.inf)
+    if big and big_means is not None:
+        unsup_x = big_means.clip(eps,np.inf)
+    else:
+        unsup_x = small_means.clip(eps,np.inf)
 
     # labeled dataset
-    sup_data, meta, split = load_cal_targets(majors_only=True,big=big,
+    sup_data, meta, split = load_cal_targets(cal_data,majors_only=True,big=big,
                                              trial=trial,hardCV=hardCV,
                                              cv_param=cv_param)
     sup_x, sup_y = sup_data
@@ -124,18 +131,13 @@ def load_mars_means(big=True):
         return np.load('examples/datasets/libs/small_means.npy')
 
 
-def load_cal_targets(majors_only=True,big=True,trial=0,hardCV=True,cv_param=3):
-    data = np.load('examples/datasets/libs/public_caltargets.npz')
-
-    x = data['data']
+def load_cal_targets(data,majors_only=True,big=True,trial=0,hardCV=True,cv_param=3):
+    x, y, majors, waves, types = data
     if majors_only:
-        y = data['target'][:,:-5]
-        majors = data['target_names'][:-5]
-    else:
-        y = data['target']
-        majors = data['target_names']
-    waves = data['data_names']
-    types = np.asarray([tup[-1] for tup in data['key']]).astype(str)
+        y = y[:,:-5]
+        majors = majors[:-5]
+
+    print('Unique Rock Types:')
     print(np.unique(types))
 
     # remove nan data
@@ -191,13 +193,6 @@ def load_cal_targets(majors_only=True,big=True,trial=0,hardCV=True,cv_param=3):
                 return (x,y),(waves,majors,types,le),split
     print('Only '+str(num)+' splits available. Returning last split.')
     return (x,y),(waves,majors,types,le),split
-
-
-def load_lanl(new=True):
-    if new:
-        return np.load('examples/datasets/libs/lanl_new.npy')
-    else:
-        raise NotImplementedError()
 
 
 def mask_norm(x,mask=True,norm=True):

examples/datasets/load_raman.py
@@ -4,6 +4,8 @@ from sklearn.model_selection import KFold
 
 from matplotlib.pyplot import cm
 
+from untapped.utilities import load_url
+
 # NOTE: the Ishikawa paper had a mistake!
 # They put Annite in the Plagioclase group,
 # but it should really be in the Mica group.
@@ -37,10 +39,9 @@ def zero_one(x):
     return (x.T/np.ptp(x,axis=1)).T
 
 
-def get_endmembers():
-    data = np.load('examples/datasets/raman/mineral_mixtures.npz')
-    x = zero_one(data['x'])
-    y = data['y']
+def get_endmembers(dataset='examples/datasets/raman/raman.pkl.gz'):
+    x, y, waves, majors = load_url('https://people.cs.umass.edu/~imgemp/datasets/raman.pkl.gz',dataset)
+    x = zero_one(x)
     endmems = np.zeros((y.shape[1],x.shape[1]))
     for i in range(y.shape[1]):
         samples = (y[:,i] == 1.)
@@ -48,13 +49,8 @@ def get_endmembers():
     return endmems
 
 
-def load_process_data(trial=0,n_folds=2,remove_mean=False,log_x=False):
-    data = np.load('examples/datasets/raman/mineral_mixtures.npz')
-    x = zero_one(data['x'])
-    y = data['y']
-    waves = data['waves']
-    majors = data['majors']
-    le = data['le'].item()
+def load_process_data(dataset='examples/datasets/raman/raman.pkl.gz',trial=0,n_folds=2,remove_mean=False,log_x=False):
+    x, y, waves, majors = load_url('https://people.cs.umass.edu/~imgemp/datasets/raman.pkl.gz',dataset)
 
     # normalize
     # preprocessing.normalize(x,norm='l1',copy=False)